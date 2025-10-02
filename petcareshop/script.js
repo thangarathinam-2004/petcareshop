944afebe9276fa26ff4347f4bd2a3710c3d4a316
Thangarathinam <thangarathinamat2004@gmail.com>
2025-10-02 21:41:01 +0530

    Add files via upload

---
+++ petcareshop/script.js
@@ -0,0 +1,19 @@
+// Example: Button Click Message
+
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    // Toggle icon between bars and times
+    const icon = menuToggle.querySelector("i");
+    if (navLinks.classList.contains("active")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
+    } else {
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
+    }
+  });
+
